52d5fc6c1e794c104b4e08ed1289986935878682
Akshat Goel <akshat@neoelect.com>
2026-05-18 00:05:54 +0530

    newcrm

--- configs.ts
+++ configs.ts
@@ -1,5 +1,10 @@
+var _INDEX = 1
+if (process.env.NEXT_PUBLIC_APP_ENV == 'local') {
+  _INDEX = 0
+}
+
 export const configs = {
-  NEO_HOST: "http://127.0.0.1:3000",
+  NEO_HOST: ["http://127.0.0.1:3000", "https://api.neoelect.com"][_INDEX],
   LOGIN_PATH: "/auth/login",
   LOGOUT_PATH: "/auth/logout",
   TELEPHONY_CALLS_PATH: "/sparktg/calls",
